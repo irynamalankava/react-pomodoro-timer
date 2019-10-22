d852e6934d18f5f3f00178420727cbe65b00b00e
ira <ira@ira>
2019-10-22 18:29:19 -0500

    Updates

---
+++ precache-manifest.5e4771b94a73593f053d23aaa7467d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04442fc2e523d87b1223763c2edbf47",
+    "url": "/react-pomodoro-timer/index.html"
+  },
+  {
+    "revision": "1d6dc22883c597795483",
+    "url": "/react-pomodoro-timer/static/css/main.eebe14b5.chunk.css"
+  },
+  {
+    "revision": "5150d281d22209f83636",
+    "url": "/react-pomodoro-timer/static/js/2.682db895.chunk.js"
+  },
+  {
+    "revision": "1d6dc22883c597795483",
+    "url": "/react-pomodoro-timer/static/js/main.711a77af.chunk.js"
+  },
+  {
+    "revision": "4dd1b587d31367274e40",
+    "url": "/react-pomodoro-timer/static/js/runtime-main.ad1d82ca.js"
+  }
+]);
